d3206f293e4d17a25d4c61eaaaffc17790bb340c
rahathossain3 <rahathossain210@gmail.com>
2022-04-03 15:01:49 +0600

    props drilling

--- src/components/Grandpa/Grandpa.js
+++ src/components/Grandpa/Grandpa.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Father from '../Father/Father';
 import Uncle from '../Uncle/Uncle';
 import Aunty from '../Aunty/Aunty';
 import './Grandpa.css'
 
 const Grandpa = () => {
-    const house = 7;
+
+    const [house, setHouse] = useState(1);
+
+    const ornament = 'Diamond Ring'
+
+    const handelBuyAHouse = () => {
+        const newHouseCount = house + 1;
+        setHouse(newHouseCount);
+    }
+
     return (
         <div className='grandpa'>
             <h4>Grandpa</h4>
+
+            <p>House : {house}
+                <button
+                    onClick={handelBuyAHouse}
+                > Buy a House </button>
+            </p>
             <section style={{ display: 'flex' }}>
-                <Father house={house}></Father>
+                <Father house={house} ornament={ornament}></Father>
                 <Aunty house={house}></Aunty>
                 <Uncle house={house}></Uncle>
             </section>
