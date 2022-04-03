950dbcb17dd96bc286f76622c3a1387c5e2cd8dd
rahathossain3 <rahathossain210@gmail.com>
2022-04-03 06:06:36 +0600

    custom hook & T-shirt cart add

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import "./Home.css";
+import Cart from '../Cart/Cart';
+import useTShirts from '../../hooks/useTShirts';
+import TShirt from '../TShirt/TShirt';
 
 const Home = () => {
+    // custom hook call
+    const [tShirts, setTShirts] = useTShirts();
+
+
     return (
-        <div>
-            <h2>This is from Home</h2>
+        <div className='home-container'>
+            <div className="tshirt-container">
+                {
+                    tShirts.map(tShirt => <TShirt
+                        key={tShirt._id}
+                        tShirt={tShirt}
+                    ></TShirt>)
+                }
+            </div>
+
+            <div className="cart-container">
+                <Cart></Cart>
+            </div>
         </div>
     );
 };
