0a73c703967fcbe281a7facb3f2951365011a53d
rahathossain3 <rahathossain210@gmail.com>
2022-04-03 07:07:02 +0600

    click handler add & duplicat item control

--- README.md
+++ README.md
@@ -23,3 +23,35 @@
 
 ## Video 2
 
+
+## Video 3
+
+## click handler add ------------
+      
+        const handleAddToCart = (selectedItem) => {
+
+<!-- jodi item cart a thake  (for duplicate reduce ) -->
+
+            const exists = cart.find(tShirts => tShirts._id === selectedItem._id);
+
+            if (!exists) {
+
+<!-- get old cart data & add new data -->
+                const newCart = [...cart, selectedItem];
+<!-- set cart data -->
+                setCart(newCart);
+            }
+            else {
+                alert('Item already added');
+            }
+        };
+
+<!-- item remove------------------------- -->
+        const handleRemoveFromCart = (selectedItem) => {
+            const rest = cart.filter(tShirts => tShirts._id !== selectedItem._id);
+
+            setCart(rest);
+        };
+
+
+
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Home.css";
 import Cart from '../Cart/Cart';
 import useTShirts from '../../hooks/useTShirts';
@@ -9,6 +9,35 @@ const Home = () => {
     const [tShirts, setTShirts] = useTShirts();
 
 
+    const [cart, setCart] = useState([]);
+
+
+    //click handler add ------------
+    const handleAddToCart = (selectedItem) => {
+        // jodi item cart a thake  (for duplicate reduce )
+        const exists = cart.find(tShirts => tShirts._id === selectedItem._id);
+
+        if (!exists) {
+
+            // get old cart data & add new data
+            const newCart = [...cart, selectedItem];
+            //set cart data
+            setCart(newCart);
+        }
+        else {
+            alert('Item already added');
+        }
+    };
+
+    // item remove-------------------------
+    const handleRemoveFromCart = (selectedItem) => {
+        const rest = cart.filter(tShirts => tShirts._id !== selectedItem._id);
+
+        setCart(rest);
+    };
+
+
+
     return (
         <div className='home-container'>
             <div className="tshirt-container">
@@ -16,14 +45,18 @@ const Home = () => {
                     tShirts.map(tShirt => <TShirt
                         key={tShirt._id}
                         tShirt={tShirt}
+                        handleAddToCart={handleAddToCart}
                     ></TShirt>)
                 }
             </div>
 
             <div className="cart-container">
-                <Cart></Cart>
-            </div>
-        </div>
+                <Cart
+                    handleRemoveFromCart={handleRemoveFromCart}
+                    cart={cart}
+                ></Cart>
+            </div >
+        </div >
     );
 };
 
